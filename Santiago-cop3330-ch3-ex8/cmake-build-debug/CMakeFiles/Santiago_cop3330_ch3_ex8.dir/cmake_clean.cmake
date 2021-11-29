433fadee15fc5474fb0b764f6566f8abcedf0413
paradoxAlpha823 <55543878+paradoxAlpha823@users.noreply.github.com>
2021-11-29 04:20:14 -0500

    Sees if value is even or odd

---
+++ Santiago-cop3330-ch3-ex8/cmake-build-debug/CMakeFiles/Santiago_cop3330_ch3_ex8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Santiago_cop3330_ch3_ex8.dir/main.cpp.obj"
+  "Santiago_cop3330_ch3_ex8.exe"
+  "Santiago_cop3330_ch3_ex8.exe.manifest"
+  "Santiago_cop3330_ch3_ex8.lib"
+  "Santiago_cop3330_ch3_ex8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Santiago_cop3330_ch3_ex8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Santiago-cop3330-ch3-ex8/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    if ( num % 2 == 0)
+        cout << "The value " << num << " is an even number.";
+    else
+        cout << "The value " << num << " is an odd number.";
+
+    return 0;
+}
